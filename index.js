34a2d800b773ae24b86aa4b6be04d1224684167b
willi <willi@DESKTOP-USN773L>
2018-11-07 01:34:41 -0500

    encode utf-8

--- index.js
+++ index.js
@@ -127,8 +127,7 @@ function sendTextMessageType(sender, bot) {
         console.log(messageData);
         // Start the request
         request({
-            url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: {access_token: token},
+            url: 'https://graph.facebook.com/v2.6/me/messages?access_token='+token,
             method: 'POST',
             json: {
                 recipient: {id: sender},
