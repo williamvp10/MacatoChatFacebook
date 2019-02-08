253d77a23fedfbd7c2a27518442397bee7708f69
willi <willi@DESKTOP-USN773L>
2019-02-07 23:18:34 -0500

    new buttons corection3

--- index.js
+++ index.js
@@ -244,7 +244,7 @@ function sendTextMessageIngredients(sender, bot) {
         elements += ' "title":"' + bot.buttons.product[i].ingredientes + '",';
         elements += ' "image_url":false,';
         elements += ' "subtitle":"null",';
-        elements += ' ""default_action": {';
+        elements += ' "default_action": {';
         elements += ' "type": "web_url",';
         elements += ' "url": "https://petersfancybrownhats.com/view?item=103",';
         elements += ' "messenger_extensions": false,';
